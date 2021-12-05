9915b5838b780c49872ce1b5109254925d3aa442
imIbroxim <ibrohm1998@gmail.com>
2021-12-05 18:41:39 +0500

    optimisation js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@ const elDarkMode = document.querySelector('.dark-mode');
 const elBtnMode = document.querySelector('.btn-mode');
 
 elBtnMode.addEventListener('click', function() {
-    document.querySelector('body').classList.add('dark-mode');
-}
-);
-elBtnMode.addEventListener('dblclick', function() {
-    document.querySelector('body').classList.remove('dark-mode');
+    document.querySelector('body').classList.toggle('dark-mode');
 }
 )
